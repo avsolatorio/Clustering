cccb4223bb6b643442ad6882b8bee6a53a5dc89f
Rico Tiongson <thericotiongson@gmail.com>
2015-04-15 21:09:28 +0800

    Created edistance.py.

---
+++ edistance.py
@@ -0,0 +1,44 @@
+import numpy as np
+
+def edit_distance(a, b, eps = 1):
+	'''
+		Implements k-bounded Damerau-Levenshtein (edit) distance
+		via epsilon (percentage of edits). Counts inserting,
+		deleting, changing, and swapping of characters.
+	'''
+	
+	n = len(a)
+	m = len(b)
+	
+	if n==0 or m==0: return max(n,m)
+	
+	# make sure second string is shorter to save space
+	if n < m:
+		a,b = b,a
+		n,m = m,n
+	
+	cur, prev = range(m+1), None
+	k = eps * max(n,m)
+	
+	for i in xrange(1, n+1):
+		cur, prev, prev2 = [i], cur, prev
+		for j in xrange(1, m+1):
+			insert = cur[j-1] + 1
+			delete = prev[j] + 1
+			change = prev[j-1] + int(a[i-1] != b[j-1])
+			swap = prev[j-2] + 1 if prev2 and a[i-2:i] == b[j-2:j] else change
+			cur.append(min(insert, delete, change, swap))
+		if min(cur) > k: return 1
+	return cur[m]
+
+def edit_ratio(a, b, eps = 1):
+	return float(edit_distance(a,b)) / max(len(a), len(b))
+	
+def edit_radius(data, eps = 1):
+	'''
+		Returns the median pairwise edit ratio.
+	'''
+	n = len(data)
+	if n <= 1: return 0
+	dist = [edit_ratio(data[i], data[j], eps) for i in xrange(n) for j in xrange(i+1, n)]
+	return np.median(dist)
